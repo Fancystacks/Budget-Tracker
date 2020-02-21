180cfbc67b1f892f069a0717705a2113a3d62d5e
Leah Migacz <lmigacz@msudenver.edu>
2020-02-21 08:39:50 -0700

    added service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
   
-  // install
+  // install phase
   self.addEventListener("install", function(evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
@@ -15,10 +15,11 @@ const FILES_TO_CACHE = [
         return cache.addAll(FILES_TO_CACHE);
       })
     );
-  
+  // go directly into activation phase
     self.skipWaiting();
   });
   
+  // activate phase
   self.addEventListener("activate", function(evt) {
     evt.waitUntil(
       caches.keys().then(keyList => {
@@ -62,7 +63,6 @@ const FILES_TO_CACHE = [
     }
   
     // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
     evt.respondWith(
       caches.match(evt.request).then(function(response) {
         return response || fetch(evt.request);
